ebaf956f2a975947e7b6821b8a4d208ca55483ad
요술토끼 <curious.wzrabbit@gmail.com>
2025-06-16 10:38:25 +0900

    fix: fixed warning verdict even if it's not warning message in stderr
    - Some languages such as C#, Python 3, stderr is shown even if it's not an error message (just a simple log), so we have to check further if it's a warning message or not

--- src/js/features/test.js
+++ src/js/features/test.js
@@ -153,6 +153,14 @@ function extendTest() {
     return { head, body, foot };
   }
 
+  function isStderrContainsWarning(language, stderr) {
+    if (!['Python 3', 'C#', 'Pascal', 'F#'].includes(language)) {
+      return stderr !== '';
+    }
+
+    return stderr.toLowerCase().includes('warning');
+  }
+
   function runTestAll() {
     for (const key of Object.keys(tcController)) {
       const { head, body, foot, input, output } = tcController[key];
@@ -161,6 +169,7 @@ function extendTest() {
       // set values
       body.innerText = '';
       foot.innerText = 'Running...';
+      const userSelectedLanguage = getUserSelectedLanguage();
       compile(input)
         .then(({ stdout, stderr }) => {
           const sameOutput = output.trim() == stdout.trim();
@@ -174,7 +183,14 @@ function extendTest() {
           if (stdout && errContent) {
             // code works but warnings
             body.innerText = `stdout:\n${stdout}\n\nstderr:\n${errContent}`;
-            isPassed = sameOutput ? 'Warning' : 'Fail';
+
+            if (!sameOutput) {
+              isPassed = 'Fail';
+            } else if (
+              isStderrContainsWarning(userSelectedLanguage, errContent)
+            ) {
+              isPassed = 'Warning';
+            }
           } else if (errContent) {
             // compile or runtime error
             body.innerText = `stderr:\n${errContent}`;
